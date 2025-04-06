a9501ce224ac1fe0cd00a38f0988305455ea8cac
sabuh_u0btff3 <sabuh_u0btff3@sabuh>
2025-04-06 23:23:20 +0900

    問題1

---
+++ HelloWorld_teraokahayato/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,42 @@
+package curriculum_A;
+
+
+
+public class Qes1_13 {
+
+	
+
+	
+	public static void main(String[] args) {
+		
+// 問題1
+
+		// バイト型の変数を宣言
+		byte baito;
+		// 短整数型の変数を宣言
+		short tan;
+		// 整数型の変数を宣言
+		int i;
+		// 長整数型の変数を宣言
+		long tyou;
+
+		// 単精度浮動小数点数型の変数を宣言
+		float f;
+		// 倍精度浮動小数点数型の変数を宣言
+		double d;
+
+		// 文字型の変数を宣言
+		char moji;
+		// 文字列型の変数を宣言
+		String retu;
+
+		// ブーリアン型の変数を宣言
+		boolean buu;
+		
+
+	}
+
+}
+
+
+
