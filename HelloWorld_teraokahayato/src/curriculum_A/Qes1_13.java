6a781729c8c826aacb35edf23ff4f15152d5e77e
sabuh_u0btff3 <sabuh_u0btff3@sabuh>
2025-04-07 12:35:55 +0900

    問題6まで

--- HelloWorld_teraokahayato/src/curriculum_A/Qes1_13.java
+++ HelloWorld_teraokahayato/src/curriculum_A/Qes1_13.java
@@ -42,28 +42,28 @@ public class Qes1_13 {
 		// 問題2
 
 		// バイト型の変数を初期化
-		baito = 20;
+		baito = 0;
 
 		// 短整数型の変数を初期化
-		tan = 300;
+		tan = 0;
 
 		// 整数型の変数を初期化
-		i = 4000;
+		i = 0;
 
 		// 長整数型の変数を初期化
-		tyou = 50000;
+		tyou = 0L;
 
 		// 単精度浮動小数点数型の変数を初期化
-		f = 0.5f;
+		f = 0.0f;
 
 		// 倍精度浮動小数点数型の変数を初期化
-		d = 11.5;
+		d = 0.0d;
 
 		// 文字型の変数を初期化
-		moji = 'あ';
+		moji = '\u0000';
 
 		// 文字列型の変数を初期化
-		retu = "ソーリー";
+		retu = "null";
 
 		// ブーリアン型の変数を初期化
 		buu = false;
@@ -122,16 +122,46 @@ public class Qes1_13 {
 
 		// 10引く100をしてコンソールに出力
 		System.out.println( baito - tan);
+		
+		// 改行
+		System.out.println("");
 
 		
 		// 問題5
 
-		String num = " 20 ";
+		String num = "20";
 		int num1 = 23;
 
 		// 文字列を数値に変換してからコンソールに出力
 		int num2 = Integer.parseInt(num);
 		System.out.println("ハローJAVA" + ( num2 + num1));
+		
+		// 改行
+		System.out.println("");
+		
+		// 問題6
+		
+		// 情報をローカル変数に代入
+		String name = "山田太郎";
+		int age = 18;
+		double height = 170.5;
+		double weight = 62.2;
+		String food = "寿司";
+		
+		// 変数を使ってコンソールに出力
+		System.out.println("「初めまして" + name + "です」");
+		
+		System.out.println("「年齢は" + age + "歳です」");
+		
+		System.out.println("「身長は" + height + "cmです」");
+		
+		System.out.println("「体重は" + weight + "kgです」");
+		
+		System.out.println("「好きな食べ物は" + food + "です」");
+		
+		// 改行
+		System.out.println("");
+				
 
 	}
 
