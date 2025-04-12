f939857ed485464b89791c8fb5c01939442ed50b
sabuh_u0btff3 <sabuh_u0btff3@sabuh>
2025-04-12 23:53:50 +0900

    問題5

--- HelloWorld_teraokahayato/src/curriculum_B/Qes5.java
+++ HelloWorld_teraokahayato/src/curriculum_B/Qes5.java
@@ -4,7 +4,17 @@ public class Qes5 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("課題5\n");
 
+		for (int i = 1; i <= 9; i++) {
+
+			for (int j = 1; j <= 20; j++) {
+
+				System.out.print(String.format(" %03d * %03d = %03d", j, i, j * i));
+
+				if (i != 10) System.out.print(" || ");
+			}
+			System.out.println();
+		}
 	}
-
 }
