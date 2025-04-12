297c78f2a4fb75499e05d44db314fcdc90805285
sabuh_u0btff3 <sabuh_u0btff3@sabuh>
2025-04-12 23:48:58 +0900

    問題4

--- HelloWorld_teraokahayato/src/curriculum_B/Qes4.java
+++ HelloWorld_teraokahayato/src/curriculum_B/Qes4.java
@@ -1,10 +1,21 @@
+
 package curriculum_B;
 
 public class Qes4 {
 
 	public static void main(String[] args) {
-		
 
+		System.out.println("課題4\n");
+
+		for (int j = 1; j <= 9; j++) {
+
+			for (int i = 1; i <= 9; i++) {
+
+				System.out.print(String.format(" %02d * %02d = %02d", j, i, j * i));
+
+				if (i != 9) System.out.print(" || ");
+			}
+			System.out.println();
+		}
 	}
-
 }
