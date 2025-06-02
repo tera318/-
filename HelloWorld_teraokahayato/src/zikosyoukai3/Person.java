0b6615a184ab515918ed249f5239b819b2dfed61
driveline_9 <driveline_9@driveline9>
2025-06-02 14:36:37 +0900

    自己紹介③完了

---
+++ HelloWorld_teraokahayato/src/zikosyoukai3/Person.java
@@ -0,0 +1,44 @@
+package zikosyoukai3;
+
+public class Person{
+	 public static int count = 0;
+	 public String firstName;
+	 public String lastName;
+	 public int age;
+	 public double height, weight;
+
+
+	 Person(String firstName, int age, double height, double weight){
+		 this.firstName = firstName;
+		 this.age = age;
+		 this.height = height;
+		 this.weight = weight;
+	 }
+
+	 Person(String firstName, String lastName, int age, double height, double weight){
+		 this.firstName = firstName;
+		 this.lastName = lastName;
+		 this.age = age;
+		 this.height = height;
+		 this.weight = weight;
+		 Person.count++;
+	 }
+	
+
+	public String fullName(){
+	  return this.firstName + this.lastName;
+	 }
+	 
+	 public void print(){
+	  System.out.println("名前は" + this.fullName() + "です");
+	  System.out.println("年は" + this.age + "です");
+	 }
+
+	 public double bmi(){
+	  return this.weight / this.height / this.height;
+	 }
+
+	 public static void printCount(){
+	  System.out.println("\n合計" + Person.count + "人です");
+	 }
+	}
